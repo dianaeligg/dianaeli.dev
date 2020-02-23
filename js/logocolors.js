9f78f04dffd760dd29a145b94a27bba096d77366
Diana García <diana.eli.gg@gmail.com>
2020-02-23 17:05:48 -0600

    added uses page

--- js/logocolors.js
+++ js/logocolors.js
@@ -2,15 +2,8 @@
 let colors = ['#f79256','#fbd1a2','#7dcfb6','#00b2ca','#1d4e89'];
 let scs;
 window.addEventListener('DOMContentLoaded', (event) => {
-    // console.log('DOM fully loaded and parsed');
-    // let scs = document.getElementsByClassName('logo-section');
-    // for (let i = 0; i < scs.length; i++) {
-    //     console.log( scs[i] );
-    //     // scs[i].style.background = 'blue';
-    //     // scs[i].innerHTML = styles[i];
-    //     scs[i].addEventListener('mouseenter', () => changeTheme(scs[i]));
-    // }
     scs = document.getElementsByClassName('logo-section');
+    console.log(scs);
     changeTheme();
     setInterval(changeTheme, 2500);
 });
@@ -20,19 +13,4 @@ function changeTheme() {
     console.log(section);
     section.style.background = colors[Math.floor(Math.random() * colors.length)];
     section.style["border-radius"] = '50%';
-    // let link = document.createElement('link');  
-    // link.rel = 'stylesheet';  
-    // link.type = 'text/css'; 
-    // link.href = 'css/'+theme+'.css';  
-    // link.id = 'sc-'+theme
-    // let head = document.getElementsByTagName('HEAD')[0];
-    // if (!document.getElementById('sc-'+theme)){
-    //     let links = document.getElementsByTagName('link');
-    //     console.log(links);
-    //     for (let i = 0; i < links.length; i++) {
-    //         if(links[i].getAttribute('id'))
-    //             document.getElementById(links[i].getAttribute('id')).remove();         
-    //     }
-    //     head.appendChild(link);  
-    // }
 }
